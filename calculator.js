a1497e24b4519c72cc1616bd9dbc4cad994ef998
Martin Viidik <MartinViidik@users.noreply.github.com>
2016-02-01 12:16:35 +0200

    Added converting cm to inches

--- calculator.js
+++ calculator.js
@@ -1,1 +1,17 @@
-// calculator.js
+// saan kätte input väljad
+window.onload = function(){
+
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  // lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click' ,convertToInch);
+
+};
+
+
+function convertToInch(){
+
+  inch.value = cm.value/2.54;
+
+}
